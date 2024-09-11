a9e4d5165beac27f053c18debad965048e2f749a
BettingIsCool <43064219+BettingIsCool@users.noreply.github.com>
2024-09-12 00:28:02 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 from config import TEXT_LANDING_PAGE
 
-min_val = st.sidebar.slider(label='Min Diff Percentage', min_value=0, max_value=25, value=2.5, step=0.5)
+min_val = st.sidebar.slider(label='Min Value Percentage', min_value=0.00, max_value=25.0, value=2.5, step=0.5)
 
 unique_leagues = db.get_leagues()
 selected_leagues = st.sidebar.multiselect(label='Leagues', options=sorted(unique_leagues), default=unique_leagues)
