72fd82117da7fc290e4c34f711b54aea88043a4c
BettingIsCool <43064219+BettingIsCool@users.noreply.github.com>
2024-09-12 00:46:22 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,10 @@ st.set_page_config(page_title="PropHunter by BettingIsCool", page_icon="🍀", l
 
 from config import TEXT_LANDING_PAGE
 
+def refresh_table():
+
+  st.cache_data.clear()
+
 min_val = st.sidebar.slider(label='Min Value Percentage', min_value=0.00, max_value=25.0, value=2.5, step=0.5)
 
 unique_leagues = db.get_leagues()
@@ -22,6 +26,7 @@ selected_books = f"({','.join(selected_books)})"
 
 bets = db.get_bets(leagues=selected_leagues, books=selected_books, min_val=float(min_val) / 100)
 
+st.button('Refresh Table', on_click=refresh_table)
 dataframe = pd.DataFrame(bets)
 
 st.write(dataframe) 
