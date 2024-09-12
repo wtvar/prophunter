2866dc79ac71a210277038cb1f764592b21f1280
BettingIsCool <43064219+BettingIsCool@users.noreply.github.com>
2024-09-12 11:59:27 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,21 @@ def refresh_table():
 
   st.cache_data.clear()
 
+
+def color_cells(val):
+
+  color = 'white'
+  if val is not None:
+    
+    if val > 0.05:
+        color = 'green'
+      elif val > 0.025:
+        color = 'yellow'
+
+  return f'color: {color}'
+
+
+
 min_val = st.sidebar.slider(label='Min Value Percentage', min_value=0.00, max_value=25.0, value=2.5, step=0.5)
 
 unique_leagues = db.get_leagues()
@@ -28,6 +43,9 @@ selected_books = f"({','.join(selected_books)})"
 bets = db.get_bets(leagues=selected_leagues, books=selected_books, min_val=float(min_val) / 100)
 
 st.button('Refresh Table', on_click=refresh_table)
-dataframe = pd.DataFrame(bets)
+bets_df = pd.DataFrame(bets)
+bets_df = bets_df.rename(columns={'starts': 'STARTS', 'league': 'LEAGUE', 'runner_home': 'HOME_TEAM', 'runner_away': 'AWAY_TEAM', 'market': 'MARKET', 'selection': 'SELECTION', 'line': 'LINE', 'odds': 'ODDS', 'fair_odds': 'FAIR_ODDS', 'book': 'BOOKMAKER', 'value': 'VALUE', 'timestamp': 'LAST_UPDATE'})
+bets_df = bets_df[['STARTS', 'LEAGUE', 'STARTS', 'HOME_TEAM', 'AWAY_TEAM', 'MARKET', 'SELECTION', 'LINE', 'ODDS', 'FAIR_ODDS', 'VALUE', 'BOOKMAKER', 'LAST_UPDATE']]
+styled_df = bets_df.style.applymap(color_cells, subset=['VALUE']).format({'LINE': '{:g}'.format, 'ODDS': '{:,.3f}'.format, 'FAIR_ODDS': '{:,.3f}'.format, 'VALUE': '{:,.2%}'.format})
 
-st.write(dataframe) 
+st.write(styled_df) 
