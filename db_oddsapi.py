9498af3ff7e8546a099f442168fbad86eeab6ac5
BettingIsCool <43064219+BettingIsCool@users.noreply.github.com>
2024-09-12 19:36:11 +0200

    Update db_oddsapi.py

--- db_oddsapi.py
+++ db_oddsapi.py
@@ -8,7 +8,7 @@ conn = st.connection('oddsapi', type='sql')
 @st.cache_data(ttl=10)
 def get_bets(leagues: str, books: str, min_val: float):
 
-  return conn.query(f"SELECT starts, league, runner_home, runner_away, market, selection, side, line, odds, fair_odds, book, region, value, timestamp FROM {TABLE_BETS} WHERE league IN {leagues} AND book IN {books} AND value >= {min_val} ORDER BY starts", ttl=600).to_dict('records')
+  return conn.query(f"SELECT starts, league, runner_home, runner_away, market, selection, side, line, odds, fair_odds, book, region, value, timestamp FROM {TABLE_BETS} WHERE league IN {leagues} AND book IN {books} AND value >= {min_val} AND starts >= NOW() ORDER BY starts", ttl=600).to_dict('records')
 
 
 @st.cache_data(ttl=10)
@@ -21,3 +21,9 @@ def get_leagues():
 def get_books():
 
   return conn.query(f"SELECT DISTINCT(book) FROM {TABLE_BETS} WHERE starts >= NOW()", ttl=600)['book'].tolist()
+
+
+@st.cache_data(ttl=10)
+def get_regions():
+
+  return conn.query(f"SELECT DISTINCT(region) FROM {TABLE_BETS} WHERE starts >= NOW()", ttl=600)['book'].tolist()
