b960e9f35cf644b14862fb53c0870125d5ef00d6
BettingIsCool <43064219+BettingIsCool@users.noreply.github.com>
2024-09-12 13:01:02 +0200

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,98 @@ TABLE_BETS = 'prophunter'
 # Display text om landing page
 TEXT_LANDING_PAGE = """
 
-Daily prop bets for NFL, NBA, NHL, MLB, NCAAF, NCAAB 🏈🏀🏒⚾️
+### YOUR DAILY DOSE OF PROP BETS FOR NFL, NBA, NHL, MLB, NCAAF, NCAAB 🏈🏀🏒⚾️
+
+⭐️ All bets listed in an easy-to-consume dashboard.
+
+⭐️ Support for 75 different prop markets!
+
+⭐️ Many bookmakers supported (see list below).
+
+⭐️ Apply filters and only list bookmakers & leagues that you are interested in.
+
+⭐️ Set minimum value threshold.
+
+⭐️ Algorithm built on @12Xpert‘s robust #WisdomOfTheCrowd strategy.
+
+⭐️ Fair odds included. This allows you to check for value at other bookmakers as well.
+
+👇 List of supported bookmakers 👇
+
+🇺🇸 US Bookmakers
+BetOnline.ag
+BetMGM
+BetRivers
+BetUS
+Bovada
+DraftKings
+FanDuel
+LowVig.ag
+MyBookie.ag
+PointsBet (US)
+SuperBook
+Unibet
+William Hill (Caesars)
+WynnBET
+betPARX
+ESPN BET
+Fliff
+Hard Rock Bet
+SI Sportsbook
+Tipico
+Wind Creek (Betfred PA)
+
+🇬🇧 UK Bookmakers
+888sport
+Betfair Exchange
+Betfair Sportsbook
+Bet Victor
+Betway
+BoyleSports
+Casumo
+Coral
+Grosvenor
+Ladbrokes
+LeoVegas
+LiveScore Bet
+Matchbook
+Mr Green
+Paddy Power
+Sky Bet
+Unibet
+Virgin Bet
+William Hill (UK)
+
+🇪🇺 EU Bookmakers
+1xBet
+888sport
+Betclic
+Betfair Exchange
+BetOnline.ag
+Betsson
+Bet Victor
+Coolbet
+Everygame
+Livescorebet (EU)
+Marathon Bet
+Matchbook
+MyBookie.ag
+NordicBet
+Suprabets
+Unibet
+William Hill
+
+🇦🇺 AU Bookmakers
+Betfair Exchange
+Betr
+BlueBet
+Ladbrokes
+Neds
+PlayUp
+PointsBet (AU)
+SportsBet
+TAB
+TopSport
+Unibet
 
 """
