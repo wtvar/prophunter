d8e5793b2f807a890b29069d2dc77e78d5bd0b7d
BettingIsCool <43064219+BettingIsCool@users.noreply.github.com>
2024-09-12 19:46:31 +0200

    Update db_oddsapi.py

--- db_oddsapi.py
+++ db_oddsapi.py
@@ -8,7 +8,7 @@ conn = st.connection('oddsapi', type='sql')
 @st.cache_data(ttl=10)
 def get_bets(leagues: str, books: str, regions: str, min_val: float):
 
-  return conn.query(f"SELECT starts, league, runner_home, runner_away, market, selection, side, line, odds, fair_odds, book, region, value, timestamp FROM {TABLE_BETS} WHERE league IN {leagues} AND book IN {books} AND region IN {regions} AND value >= {min_val} AND starts >= NOW() ORDER BY starts", ttl=600).to_dict('records')
+  return conn.query(f"SELECT starts, league, runner_home, runner_away, market, selection, side, line, odds, ref_odds, fair_odds, book, region, value, timestamp FROM {TABLE_BETS} WHERE league IN {leagues} AND book IN {books} AND region IN {regions} AND value >= {min_val} AND starts >= NOW() ORDER BY starts", ttl=600).to_dict('records')
 
 
 @st.cache_data(ttl=10)
